65789d4011f68f115e993734b1ebd93ffde162fa
whosdustin <dustin@delatore.me>
2017-11-01 12:39:39 -0500

    Update to Webpack

---
+++ README.md
@@ -0,0 +1,35 @@
+# Vue Mastery
+
+VueMastery.com: News and tutorials to reach Vue mastery
+
+- Webpack
+- ES6
+- Bulma
+- Font Awesome
+- Sass
+- Live reload using Browsersync
+
+## Getting Started
+
+Clone the repo
+```sh
+git clone https://github.com/atomjar/VM..com.git
+```
+
+Install dependencies
+```sh
+yarn
+```
+
+Run script to start devlopment
+```sh
+npm run start
+```
+
+For distribution folder run the following command and it will create a dist folder which can be placed on a server
+```sh
+npm run build
+```
+
+## License
+[MIT](LICENSE)
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+	context: path.resolve('./app'),
+	entry: './js/index.js',
+	output: {
+		path: path.resolve('./dist/'),
+		filename: 'js/bundle.js',
+		publicPath: '/',
+		libraryTarget: 'umd',
+	},
+	module: {
+		devtool: 'source-map',
+		loaders: [{
+			test: /\.js$/,
+			loader: 'babel',
+			exclude: /node_modules/,
+			query: {
+				presets: ['es2015']
+			}
+		},{
+			test: /\.html$/,
+			loader: 'html'
+		},{
+			test: /\.s[ac]ss$/,
+			loaders: ["style", "css", "sass"]
+		},{
+			test: /\.css$/,
+			loaders: ["style", "css"]
+		},{
+			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]"
+		},{
+			test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "file?name=fonts/[name].[ext]"
+		},{
+			test: /\.(jpe?g|png|gif)$/,
+  		loader:'file?name=img/[name].[ext]'
+		}]
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			template: './index.html'
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+		}),
+		new BrowserSyncPlugin({
+			server: {
+				baseDir: ['dist']
+			},
+			port: 3000,
+			host: 'localhost',
+			open: false
+		}),
+		new CopyWebpackPlugin([{
+			from: './manifest.json'
+		},{
+			from: './manifest.webapp'
+		},{
+			from: './robots.txt'
+		},{
+			from: './favicon.ico'
+		},{
+			from: './img/**/*',
+			to: './'
+		}])
+	]
+}
